18f536f464cd83a96540c011edad62498bc5f793
Adrian Cann <adriantcann@gmail.com>
2018-07-06 13:39:23 -0400

    Tap is a cool method
    * The project already has an intermittent test failure:
    
    ```
      1) Secret saves an ssn and retrieves it
         Failure/Error: expect(retrieved_secret.ssn).to eq('cat')
    
         OpenSSL::Cipher::CipherError:
           unable to set AEAD tag
         #
    /Users/adrian.cann/.rvm/gems/ruby-2.4.1/gems/encryptor-3.0.0/lib/encryptor.rb:91:in
    `auth_tag='
         #
    /Users/adrian.cann/.rvm/gems/ruby-2.4.1/gems/encryptor-3.0.0/lib/encryptor.rb:91:in
    `crypt'
         #
    /Users/adrian.cann/.rvm/gems/ruby-2.4.1/gems/encryptor-3.0.0/lib/encryptor.rb:49:in
    `decrypt'
         #
    /Users/adrian.cann/.rvm/gems/ruby-2.4.1/gems/attr_encrypted-3.1.0/lib/attr_encrypted.rb:240:in
    `decrypt'
         #
    /Users/adrian.cann/.rvm/gems/ruby-2.4.1/gems/attr_encrypted-3.1.0/lib/attr_encrypted.rb:329:in
    `decrypt'
         #
    /Users/adrian.cann/.rvm/gems/ruby-2.4.1/gems/attr_encrypted-3.1.0/lib/attr_encrypted.rb:161:in
    `block (2 levels) in attr_encrypted' u
    ```

--- secret.rb
+++ secret.rb
@@ -6,11 +6,10 @@ class Secret
   attr_encrypted :ssn, key: 'This is a key that is 256 bits!!'
 
   def self.load
-    #File.read('encrypted_ssn')
-    me = self.new
-    me.encrypted_ssn = File.read('data/encrypted_ssn')
-    me.encrypted_ssn_iv = File.read('data/encrypted_ssn_iv')
-    me
+    self.new.tap do |secret|
+      secret.encrypted_ssn = File.read('data/encrypted_ssn')
+      secret.encrypted_ssn_iv = File.read('data/encrypted_ssn_iv')
+    end
   end
 
   def save
