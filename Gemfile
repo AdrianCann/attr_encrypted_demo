3f75dbd07f494eebd6b9a7303032982631f080e9
Adrian Cann <adriantcann@gmail.com>
2018-07-06 13:32:24 -0400

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'rspec'
+gem 'attr_encrypted'
+gem 'pry'
---
+++ user.rb
@@ -0,0 +1,20 @@
+require 'attr_encrypted'
+
+class User
+  extend AttrEncrypted
+  attr_accessor :name
+  attr_encrypted :ssn, key: 'This is a key that is 256 bits!!'
+
+  def self.load
+    #File.read('encrypted_ssn')
+    me = self.new
+    me.encrypted_ssn = File.read('data/encrypted_ssn')
+    me.encrypted_ssn_iv = File.read('data/encrypted_ssn_iv')
+    me
+  end
+
+  def save
+    File.write('data/encrypted_ssn', encrypted_ssn)
+    File.write('data/encrypted_ssn_iv', encrypted_ssn_iv)
+  end
+end
